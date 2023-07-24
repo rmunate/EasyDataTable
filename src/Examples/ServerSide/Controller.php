01fbaf105890ce0e30b910ad4839cbf10adb2a6f
Grupo Altum <grupoaltum@MacBook-Pro-de-Grupo.local>
2023-07-24 13:43:27 -0500

    New Stable Version

--- src/Examples/ServerSide/Controller.php
+++ src/Examples/ServerSide/Controller.php
@@ -8,48 +8,48 @@ class Modulo extends Controller
     public function dataTable(Request $request)
     {
         $query = DB::table('novedades')
-                    ->leftJoin('tipo_novedades', 'tipo_novedades.id', '=', 'novedades.tipo_novedad_id')
-                    ->leftJoin('empleados', 'empleados.id', '=', 'novedades.empleado_id')
-                    ->select(
-                        'empleados.cedula AS identification',
-                        'empleados.nombre AS employee',
-                        'tipo_novedades.nombre AS novelty_type',
-                        'novedades.descripcion AS description',
-                        'novedades.dias_calendario AS calendar_days',
-                        'novedades.dias_habiles AS business_days',
-                        'novedades.fecha_inicial AS initial_date',
-                        'novedades.fecha_final AS final_date',
-                    );
-        
+            ->leftJoin('tipo_novedades', 'tipo_novedades.id', '=', 'novedades.tipo_novedad_id')
+            ->leftJoin('empleados', 'empleados.id', '=', 'novedades.empleado_id')
+            ->select(
+                'empleados.cedula AS identification',
+                'empleados.nombre AS employee',
+                'tipo_novedades.nombre AS novelty_type',
+                'novedades.descripcion AS description',
+                'novedades.dias_calendario AS calendar_days',
+                'novedades.dias_habiles AS business_days',
+                'novedades.fecha_inicial AS initial_date',
+                'novedades.fecha_final AS final_date',
+            );
+
         $permissionEdit = Auth::user()->can('novedades.editar'); /* (Opcional) */
 
         $datatable = new EasyDataTable();
         $datatable->serverSide();
         $datatable->request($request);
         $datatable->query($query);
-        $datatable->map(function($row) use($editar){ /* (Opcional) */
+        $datatable->map(function ($row) use ($editar) { /* (Opcional) */
             return [
                 'identification' => $row->identification,
-                'employee'       => strtolower($row->employee),
-                'novelty_type'   => strtolower($row->novelty_type),
-                'description'    => strtolower($row->description),
-                'calendar_days'  => $row->calendar_days,
-                'business_days'  => $row->business_days,
-                'initial_date'   => date('d/m/Y', strtotime($row->initial_date)),
-                'final_date'     => date('d/m/Y', strtotime($row->final_date)),
+                'employee' => strtolower($row->employee),
+                'novelty_type' => strtolower($row->novelty_type),
+                'description' => strtolower($row->description),
+                'calendar_days' => $row->calendar_days,
+                'business_days' => $row->business_days,
+                'initial_date' => date('d/m/Y', strtotime($row->initial_date)),
+                'final_date' => date('d/m/Y', strtotime($row->final_date)),
                 "action" => [
-                    "editar" => $editar
-                ]
+                    "editar" => $editar,
+                ],
             ];
         });
-        $datatable->search(function($query, $search){ /* (Opcional) */
-            return $query->where(function($query) use ($search) {
-                        $query->where('novedades.id', 'like', "%{$search}%")
-                            ->orWhere('novedades.descripcion', 'like', "%{$search}%")
-                            ->orWhere('tipo_novedades.nombre', 'like', "%{$search}%")
-                            ->orWhere('empleados.nombre', 'like', "%{$search}%")
-                            ->orWhere('empleados.cedula', 'like', "%{$search}%");
-                    });
+        $datatable->search(function ($query, $search) { /* (Opcional) */
+            return $query->where(function ($query) use ($search) {
+                $query->where('novedades.id', 'like', "%{$search}%")
+                    ->orWhere('novedades.descripcion', 'like', "%{$search}%")
+                    ->orWhere('tipo_novedades.nombre', 'like', "%{$search}%")
+                    ->orWhere('empleados.nombre', 'like', "%{$search}%")
+                    ->orWhere('empleados.cedula', 'like', "%{$search}%");
+            });
         });
         return $datatable->response();
     }
--- src/Examples/ServerSide/WithJQuery.js
+++ src/Examples/ServerSide/WithJQuery.js
@@ -1,39 +1,40 @@
 var dataTable;
 $(document).ready(function () {
-    dataTable = $('#datatable').DataTable({
-        processing: true,
-        serverSide: true,
-        responsive: true,
-        pagingType: "full_numbers",
-        ajax: { //Peticion Ajax ServerSide
-            url: baseUrl + "/modulo/datatable",
-            dataType:"JSON",
-            type:"GET"
-        },
-        columns: [
-            { data: "identification" },
-            { data: "employee" },
-            { data: "novelty_type" },
-            { data: "description" },
-            { data: "calendar_days" },
-            { data: "business_days" },
-            { data: "initial_date" },
-            { data: "final_date" },
-            { data: "action",
-                render: function (data, type, row, meta) {
-                    let btnEdit = '';
-                    if (data.editar) {
-                        btnEdit = `<button class="btn btn-sm btn-info btn-edit" data-id="${row.identification}" data-employee="${row.employee}" title="Edit">
+  dataTable = $("#datatable").DataTable({
+    processing: true,
+    serverSide: true,
+    responsive: true,
+    pagingType: "full_numbers",
+    ajax: {
+      //Peticion Ajax ServerSide
+      url: baseUrl + "/modulo/datatable",
+      dataType: "JSON",
+      type: "GET",
+    },
+    columns: [
+      { data: "identification" },
+      { data: "employee" },
+      { data: "novelty_type" },
+      { data: "description" },
+      { data: "calendar_days" },
+      { data: "business_days" },
+      { data: "initial_date" },
+      { data: "final_date" },
+      { data: "action",
+        render: function (data, type, row, meta) {
+          let btnEdit = "";
+          if (data.editar) {
+            btnEdit = `<button class="btn btn-sm btn-info btn-edit" data-id="${row.identification}" data-employee="${row.employee}" title="Edit">
                                         <i class="fa flaticon-edit-1"></i>
                                     </button>`;
-                    }
-                    return `<div class='btn-group'>${btnEdit}</div>`;
-                },
-                orderable: false
-            }
-        ],
-        language: {
-            url: "https://cdn.datatables.net/plug-ins/1.13.5/i18n/es-ES.json"
-        }
-    });
+          }
+          return `<div class='btn-group'>${btnEdit}</div>`;
+        },
+        orderable: false,
+      },
+    ],
+    language: {
+      url: "https://cdn.datatables.net/plug-ins/1.13.5/i18n/es-ES.json",
+    },
+  });
 });
